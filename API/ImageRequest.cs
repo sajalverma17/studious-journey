d03346cc4893ecbf0860f49281eb2ea3578f430a
Sajal Verma <AB20095@udnbnor.net>
2018-01-29 18:25:41 +0530

    Commit before pull

--- API/ImageRequest.cs
+++ API/ImageRequest.cs
@@ -9,13 +9,6 @@ namespace API
 {
     public class ImageRequest : IRequest<string,Bitmap>
     {
-        //string _image_url;
-
-        //public ImageRequest(string image_url)
-        //{
-        //    _image_url = image_url; 
-        //}
-
         public async Task<Bitmap> MakeRequest(string _image_url)
         {
             Bitmap album_art;
