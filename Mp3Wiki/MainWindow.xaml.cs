9379914940637563d251986b0e0c1b7387090845
sajal-home <sajalv128@gmail.com>
2018-07-04 03:00:25 +0530

    Added Border to MainWindow

--- Mp3Wiki/MainWindow.xaml.cs
+++ Mp3Wiki/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using API;
 using Data;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 
 namespace Mp3Wiki
@@ -69,7 +58,7 @@ namespace Mp3Wiki
             
         }
 
-        //Utility UI methods--
+        //Utility UI methods-- Quite slow :(
         private void SetList(List<Song> songList)
         {        
             foreach (var s in songList)
